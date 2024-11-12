f759a75d038e4b6899de8f49fd0bfaeda6718035
Pablo Pazos <pablopazosp3@gmail.com>
2024-11-12 12:40:34 +0100

    Delete WhispServer directory

--- WhispServer/src/main/java/whisp/server/ClientInterface.java
+++
@@ -1,8 +0,0 @@
-package whisp.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientInterface extends Remote {
-    void receiveMessage(String message, String senderName) throws RemoteException;
-}
